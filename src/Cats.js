a7ba4f7435046b99338b12eafab2d2a816f1f7e2
Luci Temple <lucitemple@yahoo.com.au>
2021-03-01 15:13:03 +1100

    Add styles and layout

--- src/Cats.js
+++ src/Cats.js
@@ -1,16 +1,49 @@
 
-import { Container } from "react-bootstrap";
+import { Container, Image, Row, Col } from "react-bootstrap/";
 import CatButtons from './CatButtons';
 import "./App.css";
 
 function Cats() {
   return (
     <Container>
-    <h1>Cat Web</h1>
-    <CatButtons />
-
+      <h1>Cat Web</h1>
+      <CatButtons />
+      <body className="gallery container" >
+        <Row>
+          <Col xs={6} md={4}>
+            <Image
+              src="https://www.denofgeek.com/wp-content/uploads/2020/05/Popeye-Spinach-1.jpg?resize=768%2C432"
+              thumbnail
+            />
+          </Col>
+          <Col xs={6} md={4}>
+            <Image
+              src="https://www.denofgeek.com/wp-content/uploads/2020/05/Popeye-Spinach-1.jpg?resize=768%2C432"
+              thumbnail
+            />
+          </Col>
+          <Col xs={6} md={4}>
+            <Image
+              src="https://www.denofgeek.com/wp-content/uploads/2020/05/Popeye-Spinach-1.jpg?resize=768%2C432"
+              thumbnail
+            />
+          </Col>
+          <Col xs={6} md={4}>
+            <Image
+              src="https://www.denofgeek.com/wp-content/uploads/2020/05/Popeye-Spinach-1.jpg?resize=768%2C432"
+              thumbnail
+            />
+          </Col>
+          <Col xs={6} md={4}>
+            <Image
+              src="https://www.denofgeek.com/wp-content/uploads/2020/05/Popeye-Spinach-1.jpg?resize=768%2C432"
+              thumbnail
+            />
+          </Col>
+        </Row>
+        </body>
     </Container>
-  )
+  );
 }
 
 export default Cats;
